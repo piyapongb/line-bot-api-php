6fc2cb5d2f930a2500f493dbc66ff66d7b50bee1
piyapongb <piyapongbsp@gmail.com>
2019-07-12 14:53:14 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,8 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IG+YXYoYXHnnyNt5Ugfp7c7ME85Cndf63UaJkZICfoa6bDZpEooYURSFFCU7PV+ALGK1yAUxiW+vFrHZJFNIyEmHsWLrx60Y6XMf4R7pr6+mfhGdbxO8xlpxGjPz0uGKb+J4akxmJQCIL0GikF5i6AdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'XVXY1JUFiKwCWVObaFCzYMoEud7hOzo9zkZ5oOPUg0OweXd8mtnKbKyleIxjulHhYPDIXzpO860nZLLQfXgZVnUSghVKD6ESgymOcUkML0pw+LmbQImvJ6SK3y69sfkdRrU5m1Tqu32jzp8Hqxpd0gdB04t89/1O/w1cDnyilFU=
+'; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
